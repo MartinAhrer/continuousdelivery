1a1d2e3040ecbe780f39e2850f2cf97f882f4572
Martin Ahrer <this@martinahrer.at>
2017-03-27 13:37:18 +0200

    Add release step

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -6,84 +6,98 @@
 // credentials:
 //  - docker-registry
 
-node ('docker') {
-    stage ('prepare') {
-        checkout scm
 
-        // setup volume for directory .gradle
-        try {
-            def volume=sh(returnStdout:true, script: 'docker volume ls --filter name=dot_gradle -q').trim()
-            if (!volume) {
-                sh 'docker volume create --name dot_gradle'
+def setupVolume() {
+    def volume = sh(returnStdout: true, script: 'docker volume ls --filter name=dot_gradle -q').trim()
+    if (!volume) {
+        sh 'docker volume create --name dot_gradle'
+    }
+}
+
+node('docker') {
+    def tag = env.BUILD_NUMBER
+
+    stage('prepare') {
+        checkout scm
+        setupVolume()
+    }
+    def projectDir = "${env.JENKINS_AGENT_WORKSPACE}/${env.JOB_NAME}"
+    def projectName = "${env.JOB_NAME}_${env.BUILD_NUMBER}"
+
+    def projectSettings = ["PROJECT_DIR=/${projectDir}",
+                         "COMPOSE_PROJECT_NAME=${projectName}"]
+
+    def applicationSettings = ["SPRING_DATASOURCE_URL=jdbc:postgresql://postgresdb/app",
+                               "SPRING_DATASOURCE_USERNAME=spring",
+                               "SPRING_DATASOURCE_PASSWORD=boot",
+                               "SPRING_JPA_GENERATE_DDL=true",
+                               "APP_HOST=app",
+                               "APP_PORT=8080"]
+    def releaseSettings=[]
+
+    stage('build') {
+        withEnv(projectSettings) {
+            try {
+                tag = sh(returnStdout: true, script: 'docker-compose -f src/main/docker/pipeline-build.yml run --rm releaseInfo').trim()
+                releaseSettings=["APP_TAG=${tag}"]
+
+                withEnv(releaseSettings) {
+                    sh "docker-compose -f src/main/docker/pipeline-build.yml run --rm build"
+
+                    junit("build/docsTest-results/*.xml")
+
+                    dir('build/docker') {
+                        sh "docker-compose build"
+                    }
+                }
             }
-        } catch (Exception e) {
-            currentBuild.result = 'FAILURE'
+            finally {
+                sh "docker-compose -f src/main/docker/pipeline-build.yml down -v"
+            }
         }
     }
-    def projectDir="${env.JENKINS_AGENT_WORKSPACE}/${env.JOB_NAME}"
-    def projectName="${env.JOB_NAME}_${env.BUILD_NUMBER}"
 
-    stage ('build') {
-        withEnv ([ "PROJECT_DIR=/${projectDir}",
-                   "COMPOSE_PROJECT_NAME=${projectName}" ]) {
-            sh "docker-compose -f src/main/docker/pipeline-build.yml run --rm build"
-            junit ("build/docsTest-results/*.xml")
-            //junit ("build/localIntegrationTest-results/*.xml")
-            }
-    }
+    stage('integration') {
+        withEnv(projectSettings + applicationSettings + releaseSettings) {
 
-    stage ('integration') {
-        withEnv ([ "PROJECT_DIR=/${projectDir}",
-                   "COMPOSE_PROJECT_NAME=${projectName}",
-                   "SPRING_DATASOURCE_URL=jdbc:postgresql://postgresdb/app",
-                   "SPRING_DATASOURCE_USERNAME=spring",
-                   "SPRING_DATASOURCE_PASSWORD=boot",
-                   "SPRING_JPA_GENERATE_DDL=true",
-                   ]) {
-
-            dir ('build/docker') {
+            dir('build/docker') {
                 try {
-                    sh "docker-compose config && docker-compose build"
-
                     // dependent services are brought up by the integration test and have to be shut down afterwards
                     sh "docker-compose -f docker-compose.yml -f pipeline-integration-test.yml run --rm integration-test"
-                }
-                catch (Exception ex) {
-                    currentBuild.result = 'FAILURE'
+
+                    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'scm',
+                                      usernameVariable: 'GRGIT_USER', passwordVariable: 'GRGIT_PASS']]) {
+                        sh "docker-compose -f pipeline-build.yml run --rm release"
+                    }
                 }
                 finally {
                     // take the containers down, remove volumes
                     sh "docker-compose down -v --remove-orphans"
                 }
             }
-            junit ("build/integrationTest-results/*.xml")
+            junit("build/integrationTest-results/*.xml")
         }
     }
-    stage ('deliver') {
 
-        withEnv ([ "PROJECT_DIR=/${projectDir}",
-                   "COMPOSE_PROJECT_NAME=${projectName}"
-                   ]) {
+    stage('deliver') {
+        dir('build/docker') {
+            withEnv(projectSettings + releaseSettings) {
+                withCredentials([usernamePassword(
+                        credentialsId: 'docker-registry',
+                        passwordVariable: 'DOCKER_REGISTRY_PASSWORD',
+                        usernameVariable: 'DOCKER_REGISTRY_USER')]) {
 
-            withCredentials([usernamePassword(
-                credentialsId: 'docker-registry',
-                passwordVariable: 'DOCKER_REGISTRY_PASSWORD',
-                usernameVariable: 'DOCKER_REGISTRY_USER')]) {
-
-                sh 'docker login --username=${DOCKER_REGISTRY_USER} --password=${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY}'
-                dir ('build/docker') {
+                    sh 'docker login --username=${DOCKER_REGISTRY_USER} --password=${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY}'
                     try {
-                        sh "docker-compose config && docker-compose push"
-                    }
-                    catch (Exception ex) {
-                        currentBuild.result = 'FAILURE'
+                        sh "docker-compose push"
                     }
                     finally {
-                        // remove images
-                        sh "docker-compose down --rmi all"
+                        // remove local docker objects (networks, containers, images, ...)
+                        sh "docker-compose down -v --remove-orphans --rmi all"
                     }
                 }
             }
         }
     }
 }
+
--- src/infrastructure/docker/up.sh
+++ src/infrastructure/docker/up.sh
@@ -4,7 +4,7 @@ set -x
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export JENKINS_URL=http://$(ipconfig getifaddr en0):8080
-export JENKINS_AGENT_WORKSPACE=${PWD}/../../../build/jenkins-agent
+export JENKINS_AGENT_WORKSPACE=${PWD}/../../../jenkins-agent
 
 docker-compose up -d
 # This fixes the permissions on the nexus-docker-data volume.
