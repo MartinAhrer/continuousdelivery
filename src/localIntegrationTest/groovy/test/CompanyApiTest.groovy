6076b9ae2af9113d48f6dced789c59a8c61c305f
Martin Ahrer <martin.ahrer@software-craftsmen.at>
2018-08-03 15:01:27 +0200

    Upgrade Spring Boot

--- src/localIntegrationTest/groovy/test/CompanyApiTest.groovy
+++ src/localIntegrationTest/groovy/test/CompanyApiTest.groovy
@@ -3,9 +3,9 @@ package test
 import at.martinahrer.cd.Application
 import groovy.json.JsonBuilder
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
